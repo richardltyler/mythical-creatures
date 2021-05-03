f59f204387cc31a14d31a8db9ca0106bfb0fd1d5
Richard Tyler <richardtyler2011@gmail.com>
2021-05-03 12:39:55 -0600

    Complete medusa with person and statue

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,20 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    const statue = new Statue(victim.name);
+    this.statues.push(statue);
+
+    if (this.statues.length > 3) {
+      return new Person(this.statues.shift().name, 'relieved');
+    }
+  }
+}
+
+module.exports = Medusa;
---
+++ mythical-creatures/exercises/person.js
@@ -0,0 +1,8 @@
+class Person {
+  constructor(name, mood) {
+    this.name = name;
+    this.mood = mood || 'frightened';
+  }
+}
+
+module.exports = Person;
