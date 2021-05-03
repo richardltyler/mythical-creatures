dbef6bd5c3a4a722765adb77dc8fe4ef38d8a547
Richard Tyler <richardtyler2011@gmail.com>
2021-05-03 12:56:21 -0600

    Complete werewolf with vicitm

---
+++ mythical-creatures/exercises/werewolf.js
@@ -0,0 +1,32 @@
+var Victim = require('./victim');
+
+class Werewolf {
+  constructor(name) {
+    this.name = name;
+    this.form = 'human';
+    this.hungry = false;
+  }
+
+  completeTransformation() {
+    if (this.form === 'human') {
+      this.form = 'wolf';
+      this.hungry = true;
+      return 'Aaa-Woooo!';
+    } else {
+      this.form = 'human';
+      this.hungry = false;
+      return 'Where are I?';
+    }
+  }
+
+  eatVictim(victim) {
+    if (this.form === 'wolf') {
+      victim.alive = false;
+      this.form = 'human';
+      return'Yum, Hannah was delicious.';
+    } else {
+      return 'No way am I eating Baby, I\'d like a burger!'
+    }
+  }
+}
+ module.exports = Werewolf;
