db1e52862f2d6ead5a51ab87bb9a0703d86bd6c6
Richard Tyler <richardtyler2011@gmail.com>
2021-05-03 11:24:31 -0600

    Complete dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.mealsCount = 0;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.mealsCount++;
+
+    if (this.mealsCount > 2) {
+      this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon;
