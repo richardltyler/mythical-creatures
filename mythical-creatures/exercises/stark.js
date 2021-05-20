f4d77deb754f9af53fcbf3179062c44051e9f9b4
Richard Tyler <richardtyler2011@gmail.com>
2021-05-20 11:07:11 -0600

    Add notes on callDirewolf

--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -19,10 +19,17 @@ class Stark {
   }
 
   callDirewolf(wolf, location) {
-    const direwolf = new Direwolf(wolf, this.location);
+    const direwolf = new Direwolf(wolf, location);
+    direwolf.home = this.location;
     direwolf.protect(this)
-    
     return direwolf;
+    // This looks a bit clunky, but it follows the test
+    // Logically, IRL (well like in Westoros life), a Stark would call for their direwolf who would be in another location and then the direwold would have to make it to the stark's location
+    // It can almost make sense to instantiate the Direwolf with a different location and then have to call it's location to whatever the Stark's location is
+    // here's how I would do it without this logic and the tests to throw me off a lil:
+    // const direwolf = new Direwolf(wolf, this.location);
+    // direwolf.protect(this)
+    // return direwolf;
   }
 }
 
