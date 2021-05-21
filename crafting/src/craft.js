77ed9437dab2d40b07aa056c5785d743b909b7e2
Richard Tyler <richardtyler2011@gmail.com>
2021-05-21 10:36:30 -0600

    Complete Craft

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,23 @@
 class Craft {
+  constructor({ type, materials }) {
+    this.name = type;
+    this.materials = materials;
+    this.completed = false;
+  }
 
+  completeCraft() {
+    this.completed = true;
+
+    return 'All done! It looks great!';
+  }
+
+  calculateProjectTotal() {
+    return this.materials.reduce((acc, material) => {
+      acc += material.calculateMaterialCost();
+
+      return acc;
+    }, 0)
+  }
 }
 
 module.exports = Craft;
