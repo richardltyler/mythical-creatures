a081447d1c6fc9e3b3de3bb88aefbbf358db6240
Richard Tyler <richardtyler2011@gmail.com>
2021-05-21 10:42:31 -0600

    Complete Snack

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,23 @@
 class Snack {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.itemsInStock = 0;
+  }
 
+  stockItems(amount) {
+    this.itemsInStock += amount;
+  }
+
+  removeItem() {
+    if (!!this.itemsInStock) {
+      this.itemsInStock--;
+  
+      return `Item taken! There are now ${this.itemsInStock} items left.`;
+    } else {
+      return `Sorry, no ${this.name} left in stock!`
+    }
+  }
 }
 
 module.exports = Snack;
