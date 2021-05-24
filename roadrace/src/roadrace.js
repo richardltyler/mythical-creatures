e6e6e3036f61128986b807330c3d453b1a4deab6
Richard Tyler <richardtyler2011@gmail.com>
2021-05-24 10:55:06 -0600

    Completed roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,25 @@
 class Roadrace {
+  constructor({ title, city }) {
+    this.name = title;
+    this.location = city;
+    this.participants = [];
+  }
 
+  setDistance(miles) {
+    this.distance = miles;
+    
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+
+  completeRace() {
+    this.participants.forEach(runner => {
+      runner.runRace(this.name, this.distance);
+    })
+  }
 }
 
 module.exports = Roadrace;
