6f455bae48683b22f2ec589a8fab3e18b771b115
Richard Tyler <richardtyler2011@gmail.com>
2021-05-26 11:09:15 -0600

    Complete being

--- spaceport/src/being.js
+++ spaceport/src/being.js
@@ -1,4 +1,14 @@
 class Being {
+  constructor(name, species) {
+    this.name = name;
+    this.isAlive = true;
+    this.species = species;
+    this.credits = 0;
+  }
+
+  updateCredits(amount) {
+    this.credits += amount;
+  }
 }
 
 module.exports = Being;
