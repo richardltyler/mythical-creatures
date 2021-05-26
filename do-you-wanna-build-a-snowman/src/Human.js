14adcacf3eeda785de677aab82675970081cd1ef
Richard Tyler <richardtyler2011@gmail.com>
2021-05-26 11:03:33 -0600

    Complete snowman

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,36 @@
 var Snowman = require('./Snowman');
 
 class Human {
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    };
+  };
 
-}
+  gatherMaterials(material, amount) {
+    this.materials[material] += amount;
+  };
+
+  buildASnowman() {
+    this.snowman = new Snowman(this.materials);
+    return this.snowman;
+  };
+
+  placeMagicHat() {
+    this.snowman.canWearMagicHat();
+
+    if (this.snowman.magicHat) {
+      return 'Woah, this snowman is coming to life!';
+
+    } else {
+      return 'I guess I didn\'t build it correctly.';
+    };
+  };
+};
 
 module.exports = Human;
