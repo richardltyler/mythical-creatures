3bafbfc504662491720fcd5acf61d6a6a828505c
Richard Tyler <richardtyler2011@gmail.com>
2021-05-25 22:44:10 -0600

    Complete lift

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,36 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(limit) {
+    this.inService = true;
+    this.limit = limit;
+    this.skiers = [];
+    this.safetyBar = 'up';
+  }
+
+  admitSkier(name, hasTicket) {
+    if (this.skiers.length < this.limit && hasTicket) {
+      const skier = new Skier(name, hasTicket);
+
+      this.skiers.push(skier);
+    } else if (hasTicket) {
+      return `Sorry, ${name}. Please wait for the next lift!`;
+    } else {
+      return `Sorry, ${name}. You need a lift ticket!`;
+    }
+    
+    
+  }
+  startLift() {
+    if (this.skiers.length >= this.limit) {
+      this.safetyBar = 'down';
+      
+    } else {
+      const remainder = this.limit - this.skiers.length;
+
+      return remainder === 1 ? `We still need ${remainder} more skier!` : `We still need ${remainder} more skiers!`;
+    }
+  }
 }
 
 module.exports = Lift;
