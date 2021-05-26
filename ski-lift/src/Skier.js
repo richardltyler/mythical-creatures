73b95c1dd0fa464ccb6f32e18591c2cef7916fbc
Richard Tyler <richardtyler2011@gmail.com>
2021-05-25 22:35:42 -0600

    Complete skier

--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,5 +1,24 @@
 class Skier {
+  constructor(name, hasTicket) {
+    this.name = name;
+    this.hasLiftTicket = hasTicket;
+    this.skillLevel = 1;
+    this.nextSlope = 'green circle';
+  };
 
+
+  takeLesson() {
+    this.skillLevel++;
+  };
+
+  pickSlope() {
+    if (this.skillLevel > 4) {
+      this.nextSlope = 'black diamond';
+
+    } else if (this.skillLevel > 2) {
+      this.nextSlope = 'blue square';
+    };
+  };
 }
 
 module.exports = Skier;
