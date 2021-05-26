2dfe3449ed8e0fc5b8ead81f27373a6e4a0468dd
Richard Tyler <richardtyler2011@gmail.com>
2021-05-26 10:56:00 -0600

    Complete snowman

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,21 @@
 class Snowman {
+  constructor({carrots, coal, buttons, snowballs}) {
+    this.carrots = carrots;
+    this.coal = coal;
+    this.buttons = buttons;
+    this.snowballs = snowballs;
+    this.magicHat = false;
+  };
 
-}
+  canWearMagicHat() {
+    if (this.coal > 1 
+        && this.buttons > 4 
+        && this.carrots > 0 
+        && this.snowballs > 1) {
+
+      this.magicHat = true;
+    };
+  };
+};
 
 module.exports = Snowman;
