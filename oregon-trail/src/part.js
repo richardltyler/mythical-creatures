7e2dc257b6875b493344d7f5a04b7931901e9f72
Richard Tyler <richardtyler2011@gmail.com>
2021-05-26 18:31:44 -0600

    Complete Part

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -1,5 +1,24 @@
 class Part {
+  constructor(type) {
+    this.type = type;
+    this.broken = false;
+    this.quality = 2;
+  }
 
+  break() {
+    this.broken = true;
+    this.quality--;
+  }
+
+  repair() {
+    if (this.quality > 0) {
+      this.broken = false;
+
+    } else {
+      
+      return `This ${this.type} has seen better days, we\'ll need a brand new one!`
+    }
+  }
 }
 
 module.exports = Part;
