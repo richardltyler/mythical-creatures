99cd7005b93b750e961c01821b0e1db5c872ef67
Richard Tyler <richardtyler2011@gmail.com>
2021-05-26 18:28:23 -0600

    Complete Gear

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,26 @@
 class Gear {
+  constructor(type, quantity) {
+    this.type = type;
+    this.quantity = quantity;
+  };
 
+  checkForValidity() {
+    const validTypes = [
+      'food', 
+      'ammunition', 
+      'clothes'
+    ];
+
+    if (!validTypes.includes(this.type)) {
+      const invalid = this.type;
+      this.type = null;
+      this.quantity = 0;
+      return `I don\`t think a ${invalid} will help us.`;
+    } else {
+    
+      return `Great, we\'ll need lots of ${this.type}!`;
+    };
+  };
 }
 
 module.exports = Gear;
