878cc5d14c0b71f8a5cc7ff5b1f7d48a24bde0a5
Richard Tyler <richardtyler2011@gmail.com>
2021-05-31 18:11:32 -0600

    Complete Snack

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,23 @@
 class Snack {
+  constructor(type) {
+    this.deliciousLevel = 'extra';
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  };
 
-}
+  getEaten() {
+    this.amount -= 10;
+
+    if (this.amount <= 20) {
+      this.cuttingItClose = true;
+    };
+  };
+
+  checkForHealthy() {
+    return this.type.toLowerCase().includes('fruit');
+  };
+};
 
 module.exports = Snack;
