3f4e5fe848273b8925fdb2ff329d046307bc6c70
Richard Tyler <richardtyler2011@gmail.com>
2021-05-31 18:19:32 -0600

    Complete lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,27 @@
 class LunchBox {
+  constructor({ owner, madeOf, shape, color}) {
+    this.owner = owner;
+    this.material = madeOf;
+    this.shape = shape;
+    this.color = color;
+    this.snacks = [];
+  };
 
+  acquireSnack(snack) {
+    snack.isInLunchBox = true;
+    this.snacks.push(snack);
+  };
+
+  findHealthySnacks() {
+    return this.snacks.reduce((acc, snack) => {
+      !!snack.checkForHealthy()
+        && acc.push(snack.type);
+      
+      return acc;
+    }, [])
+
+
+  }
 }
 
 module.exports = LunchBox;
